b0f1b399ccb9b0f3a8620dc938890bc88b33c7c5
박지윤 <jyun1937@naver.com>
2023-07-21 09:42:17 +0900

    fix router

--- frontend/src/utils/loginCheck.js
+++ frontend/src/utils/loginCheck.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const loginCheck = async () => {
   const { data } = await axios({
-    url: `http://54.180.26.199/auth/login_check`,
+    // url: `http://54.180.26.199/auth/login_check`,
+    url: `http://localhost:8080/auth/login_check`,
     method: "get",
   });
   return data.code;
